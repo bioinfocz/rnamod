08983b39532dd012d7293d69598cc53235d13b88
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2018-05-17 13:51:44 +0200

    summary

--- rnamod/position_data.py
+++ rnamod/position_data.py
@@ -1,15 +1,21 @@
+import math
 import collections
 from scipy import stats
+
+MIN_COVERAGE = 100
+MIN_PVALUE = 0.1
+MIN_STOPS_RELATIVE = 5
+MIN_ERRORS_RELATIVE = 5
 
 class PositionData:
    def __init__(self, base, position):
       self.base = base
       self.position = position
       self.datasets = collections.OrderedDict()
-      self.pattern_matches = []
+      self.patterns_matched = []
 
    def add_pattern_match(self, pattern):
-      self.pattern_matches.append(pattern)
+      self.patterns_matched.append(pattern)
 
    def dataset(self, name):
       return self.datasets[name]
@@ -30,5 +36,35 @@ class PositionData:
             experiments_stops.append(dataset.stops_coverage_relative)
             experiments_errors.append(dataset.errors_relative)
 
-      _, self.pvalue_stops = stats.ttest_ind(experiments_stops, checks_stops)
-      _, self.pvalue_errors = stats.ttest_ind(experiments_errors, checks_errors)
+      self.statistic_stops, self.pvalue_stops = stats.ttest_ind(experiments_stops, checks_stops)
+      self.statistic_errors, self.pvalue_errors = stats.ttest_ind(experiments_errors, checks_errors)
+
+   def is_stops_significant(self):
+      if math.isnan(self.pvalue_stops) or self.pvalue_stops > MIN_PVALUE:
+         return False
+
+      for _, dataset in self.datasets.items():
+         if dataset.stops_coverage > MIN_COVERAGE and dataset.stops_coverage_relative > MIN_STOPS_RELATIVE:
+            return True
+
+      return False
+
+   def is_errors_significant(self):
+      if math.isnan(self.pvalue_errors) or self.pvalue_errors > MIN_PVALUE:
+         return False
+
+      for _, dataset in self.datasets.items():
+         if dataset.coverage > MIN_COVERAGE and dataset.errors_relative > MIN_ERRORS_RELATIVE:
+            return True
+
+      return False
+
+   def is_significant(self):
+      if len(self.patterns_matched) > 0:
+         return True
+
+      if self.is_stops_significant() or self.is_errors_significant():
+         return True
+
+      return False
+
